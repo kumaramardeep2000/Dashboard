acb6663082105dc0097416a83aa470acf7834935
Amardeep Kumar Saw <kumaramardeep2000@gmail.com>
2024-12-13 22:20:03 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify } from 'react-icons/bs'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className='header'>
+    <div className='menu-icons'>
+      <BsJustify className='icon'/>
+    </div>
+    <div className='header-left'>
+      <BsSearch className='icon'/>
+    </div>
+    <div className='header-right'>
+      <BsFillBellFill className='icon'/>
+      <BsFillEnvelopeFill className='icon'/>
+      <BsPersonCircle className='icon'/>
+    </div>
+    </header>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import {BsCart3, BsFillArchiveFill, BsFillGearFill, BsFillGrid3X3GapFill, BsGrid1X2Fill, BsListCheck, BsMenuButtonWideFill, BsPeopleFill } from 'react-icons/bs'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <aside id='sidebar'>
+      <div className='sidebar-title'>
+        <div className='sidebar-brand'>
+        <BsCart3 className='icon-header'/>SHOP
+        </div>
+        <span className='icon close_icon'>X</span>
+      </div>
+
+      <ul className='sidebar-list'>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsGrid1X2Fill className='icon'/> Dashboard
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillArchiveFill className='icon'/> Products
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillGrid3X3GapFill className='icon'/> Categories
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsPeopleFill className='icon'/> Customers
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsListCheck className='icon'/> Inventory
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsMenuButtonWideFill className='icon'/> Reports
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillGearFill className='icon'/> Setting
+                </a>
+            </li>
+        </ul>  
+    </aside>
   )
 }
 
